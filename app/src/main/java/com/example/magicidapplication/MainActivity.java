e0aa1fba1cf5b9ec225f56da068de34ec1b2db64
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-04-04 13:32:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/magicidapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.magicidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etID;
+    Button btnSubmit;
+    TextView tvResults;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etID = findViewById(R.id.etID);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        tvResults = findViewById(R.id.tvResults);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String idNumber = etID.getText().toString().trim();
+
+                String dob = idNumber.substring(0,6);
+
+                int gender = idNumber.charAt(6);
+
+                
+
+            }
+        });
+    }
+}
